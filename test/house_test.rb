6f76358d473ec6d03402f7abf829111dd27fabc2
Michael Evans <michaeljevans113@gmail.com>
2020-05-13 10:41:31 -0600

    Add test for rooms_sorted_by_area method

--- test/house_test.rb
+++ test/house_test.rb
@@ -67,7 +67,7 @@ class HouseTest < MiniTest::Test
     house.add_room(room_3)
     house.add_room(room_4)
 
-    assert_equal 1900, house.area
+    assert_equal 1900, house.area_of_house
   end
 
   def test_house_details_displayed_correctly
@@ -90,5 +90,21 @@ class HouseTest < MiniTest::Test
     house.add_room(room_4)
 
     assert_equal 210.53, house.price_per_square_foot
+  end
+
+  def test_rooms_are_sorted_by_area
+    house = House.new("$400000", "123 sugar lane")
+
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal [room_4, room_3, room_2, room_1], house.rooms_sorted_by_area
   end
 end
